3d761c95ac1b0e235e38ebb6774e0b6d5521d134
xiaobao@shuanger <xiaobaoshuanger@xiaobaoshuangerdeMacBook-Pro.local>
2018-08-04 11:13:36 +0800

    资源管理

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import _ from 'lodash'
 import './style.css'
 import Icon from './potato.jpg'
+import Data from './data.xml';
 function component() {
     var element = document.createElement('div');
   
@@ -11,6 +12,7 @@ function component() {
     let myIcon = new Image()
     myIcon.src=Icon
     element.appendChild(myIcon)
+    console.log(Data)
   
     return element;
   }
--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,28 @@ module.exports = {
            'file-loader'
          ]
          // 压缩和优化你的图像。查看 image-webpack-loader 和 url-loader，
-       }
+      },
+       //file-loader 和 url-loader 可以接收并加载任何文件，然后将其输出到构建目录
+      {
+          test: /\.(woff|woff2|eot|ttf|otf)$/,
+          use: [
+            'file-loader'
+          ]
+      },
+      // 处理csv tsv格式的文件
+      {
+        test: /\.(csv|tsv)$/,
+        use: [
+          'csv-loader'
+        ]
+      },
+      // 处理XML
+      {
+          test: /\.xml$/,
+          use: [
+            'xml-loader'
+          ]
+      }
     ]
   }
 };
